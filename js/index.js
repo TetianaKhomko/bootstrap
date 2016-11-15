03b9f4ca07c72970202e70657373ee71054ed857
Tetiana Khomko <arnelli.tt@gmail.com>
2016-11-15 18:39:09 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function() {
+  $(document).on('click','.navbar-collapse.in',function(e) {
+      if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+          $(this).collapse('hide');
+      }
+  });
+});
+
+
+$(function() {
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 2000);
+        }
+    });
+});
